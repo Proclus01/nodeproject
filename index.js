8c4d93ac97b28f13662c992a28e60a3e382ba3f7
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-21 23:50:09 -0700

    test of require functionality

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 //jshint esversion:9
 
-console.log('hi there!');
+//****************************************************************************************
+//
+//      The require function takes a path or library name as its input param
+//      and then returns the value that is exported from the file in the path
+//      Finally, we capture that value returned inside of a variable in our own JS file
+//      This is how the module system works
+//
+// ****************************************************************************************
+
+const message = require('./myscript.js'); // the ./ refers to a relative path
+
+console.log(message);
 
