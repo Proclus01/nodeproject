89b31e6ef47b56b4d23195ab3a8ca08d5ad64716
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-23 23:40:29 -0700

    changed readdir file path to process.cwd() for generality

--- index.js
+++ index.js
@@ -10,8 +10,13 @@
 
 const fs = require('fs'); // require filesystem
 
-// read directory
-fs.readdir('.', (err, filenames) => { // 2 callback variables: error, filenames
+// read current directory:
+//
+//      First parameter is PATH, initially we tested it with '.' 
+//      but now we replace the static path with process.cwd(),
+//      this lets us run our node app from anywhere in the file system.
+//
+fs.readdir(process.cwd(), (err, filenames) => { // 2 callback variables: error, filenames
     if (err) {
         // error handling code here
         console.log(err);
