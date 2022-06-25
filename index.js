1b4b101c56bf08b69656a5fd71fe87dca8cf8cca
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-25 16:39:50 -0700

    implement dynamic filename and path arguments

--- index.js
+++ index.js
@@ -10,14 +10,21 @@
 //      Implement the 'ls' command using Node including 'ls -a' and 'ls'
 //      Run program in terminal using 'nls' and it automatically fetches file path
 //      Files and folders are coloured differently when read in terminal
+//      You can pass filepaths as command line arguments into nls
+//      in order to read directories beyond the cwd
 //
 // ****************************************************************************************
 
 import fs from 'fs'; // require filesystem to get <stats> object and operate on it
 import chalk from 'chalk'; // require chalk for console coloring
+import path from 'path'; // require path to help put together directory path
 
 // Handling lstat with promises using fs.promises from fs
 const { lstat } = fs.promises;
+
+// If no argument, use cwd as argument for readdir
+// if argument, use argv array and pass user input to readdir
+const targetDir = process.argv[2] || process.cwd();
 
 // read current directory:
 //
@@ -25,7 +32,7 @@ const { lstat } = fs.promises;
 //      but now we replace the static path with process.cwd(),
 //      this lets us run our node app from anywhere in the file system.
 //
-fs.readdir(process.cwd(), async (err, filenames) => { // 2 callback variables: error, filenames
+fs.readdir(targetDir, async (err, filenames) => { // 2 callback variables: error, filenames
     if (err) {
         // Error handling code here
         console.log(err);
@@ -34,7 +41,9 @@ fs.readdir(process.cwd(), async (err, filenames) => { // 2 callback variables: e
     // Map over the filenames array
     // For each file name, call lstat and return the promise that gets created when we call it
     const statPromises = filenames.map( filename => {
-        return lstat(filename);
+        return lstat(
+            path.join(targetDir, filename) // join the filename with the target directory
+            );
     });
 
     // Promise.all will wait for all operations to resolve 
