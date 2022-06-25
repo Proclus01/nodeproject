32c742203478c4a6a75371e0858abad25e9f3739
Neven <95269427+Proclus01@users.noreply.github.com>
2022-06-25 15:36:21 -0700

    clear readdir code block for refactor to promise

--- index.js
+++ index.js
@@ -27,46 +27,5 @@ fs.readdir(process.cwd(), (err, filenames) => { // 2 callback variables: error, 
         console.log(err);
     }
 
-    // Create array with length = filenames
-    // and fill the values with null
-    // This array will store the results of lstat
-    const allStats = Array(filenames.length).fill(null); 
-
-    // Iterate over every filename and call lstat to get <stats> object for filename
-    // then inspect stats object and validate
-    for (let filename of filenames) {
-        // Get index from for...of loop
-        const index = filenames.indexOf(filename);
-
-        // Inspect filename using lstat and then validate <stat> object returned
-        fs.lstat(filename, (err, stats) => {
-            if (err) {
-                // error handling code here
-                console.log(err);
-            }
-        
-            // Push value at index into allStats array
-            allStats[index] = stats;
-
-            // Check if any value inside allStats == null
-            const ready = allStats.every(
-                // pass in iterator function to call for every element inside allstats
-                // iterator returns boolean value
-                (stats) => {
-                    return stats;
-                }
-            );
-
-            // If every value in allStats is not null, proceed to next step:
-            //      Check if the scanned file is a file or folder and log the name
-            if (ready) {
-                allStats.forEach(
-                    (stats, index) => {
-                        console.log(filenames[index], stats.isFile());
-                    }
-                );
-            }
-
-        }); //lstat end
-    } // for...of end
+    //
 }); // readdir end
